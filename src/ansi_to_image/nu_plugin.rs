2e5096afe154a70f4c3a8da791fa002a6b8be97e
Motalleb Fallahnezhad <fmotalleb@gmail.com>
2023-12-01 17:56:04 +0330

    `to png` params stability

--- src/ansi_to_image/nu_plugin.rs
+++ src/ansi_to_image/nu_plugin.rs
@@ -1,19 +1,45 @@
 use nu_plugin::{EvaluatedCall, LabeledError};
-use nu_protocol::Value;
+use nu_protocol::{Span, Value};
 
 use super::ansi_to_image::make_image;
 
 pub fn ansi_to_image(call: &EvaluatedCall, input: &Value) -> Result<Value, LabeledError> {
-    let i = input.as_binary().unwrap();
-    let size = match call.get_flag_value("width") {
-        Some(val) => Some(val.as_int().unwrap() as u32),
-        None => None,
+    let i: &[u8] = match input.as_binary().ok() {
+        Some(value) => value,
+        _ => {
+            return Err(make_params_err(
+                "cannot read input as binary data (maybe its empty)".to_string(),
+                Some(input.span()),
+            ))
+        }
     };
-    let out = call
-        .get_flag_value("output-path")
-        .unwrap()
-        .as_path()
-        .unwrap();
+    let size = match call.get_flag_value("width") {
+        Some(val) => match val.as_int().ok() {
+            Some(value) => Some(value as u32),
+            _ => None,
+        },
+        _ => None,
+    };
+
+    let out = match call.get_flag_value("output-path").map(|i| i.as_path()) {
+        Some(path) if path.is_ok() => path.unwrap(),
+        _ => {
+            return Err(make_params_err(
+                "`--output-path` parameter is not correct file path value".to_string(),
+                Some(call.head),
+            ))
+        }
+    };
+
     make_image(out.as_path(), size, i);
+
     Ok(Value::nothing(call.head))
+}
+
+fn make_params_err(text: String, span: Option<Span>) -> LabeledError {
+    return LabeledError {
+        label: "faced an error when tried to parse the params".to_string(),
+        msg: text,
+        span: span,
+    };
 }
